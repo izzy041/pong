12a5017ca22d08c5a1dda860072c17c165a844b9
Izzy <isamede3@gmail.com>
2024-03-18 22:09:11 +0000

    Added an opponent player.

--- src/main.cpp
+++ src/main.cpp
@@ -4,25 +4,6 @@
 const int windowWidth = 800;
 const int windowHeight = 450;
 
-// Player variables
-Rectangle player { 30.0f, (float) windowHeight / 2.0f,  10.0f, 30.0f};
-const float playerSpeed = 5.0f;
-int playerScore = 0;
-
-// Handles player movement
-void MovePlayer()
-{
-    // Move up if w pressed and not at the top of the screen
-    // Move down if s pressed and not at the bottom of the screen
-    if (IsKeyDown(KEY_W) && player.y > 0.0f)
-        player.y -= playerSpeed;
-    else if (IsKeyDown(KEY_S) && player.y < windowHeight - player.height)
-        player.y += playerSpeed;
-}
-
-// Opponent variables
-int opponentScore = 0;
-
 // Ball variables
 Vector2 ballPosition = { (float) windowWidth / 2.0f, (float) windowHeight / 2.0f };
 Vector2 ballSpeed { 5.0f, 4.0f };
@@ -43,11 +24,44 @@ void MoveBall()
         ballSpeed.y *= -1.0f;
 }
 
+// Player variables
+Rectangle player { 30.0f, (float) windowHeight / 2.0f,  10.0f, 30.0f};
+const float playerSpeed = 5.0f;
+int playerScore = 0;
+
+// Handles player movement
+void MovePlayer()
+{
+    // Move up if w pressed and not at the top of the screen
+    // Move down if s pressed and not at the bottom of the screen
+    if (IsKeyDown(KEY_W) && player.y > 0.0f)
+        player.y -= playerSpeed;
+    else if (IsKeyDown(KEY_S) && player.y < windowHeight - player.height)
+        player.y += playerSpeed;
+}
+
+// Opponent variables
+Rectangle opponent {windowWidth - 40.0f, (float) windowHeight / 2.0f, 10.0f, 30.0f};
+const float opponentSpeed = 3.6f;
+int opponentScore = 0;
+
+// Moves the opponent, tracking the y position of the ball
+void MoveOpponent()
+{
+    if (opponent.y < ballPosition.y)
+        opponent.y += opponentSpeed;
+    else if (opponent.y > ballPosition.y)
+        opponent.y -= opponentSpeed;
+}
+
 // Checks for collisions and handles them
 void HandleCollision()
 {
     // If the player collides with the ball, bounce the ball off of the player
     if (CheckCollisionCircleRec(ballPosition, ballRadius, player))
+        ballSpeed.x *= -1.0f;
+    // If the opponent collides with the ball, bounce the ball off of the opponent
+    if (CheckCollisionCircleRec(ballPosition, ballRadius, opponent))
         ballSpeed.x *= -1.0f;
 }
 
@@ -68,6 +82,9 @@ int main()
 
         MoveBall();
         DrawCircleV(ballPosition, ballRadius, WHITE);
+
+        MoveOpponent();
+        DrawRectangleRec(opponent, WHITE);
 
         HandleCollision();
 
