f396cfc50922c6977963c68ef725e61c6a34356f
Izzy <isamede3@gmail.com>
2024-03-18 21:54:01 +0000

    Added a moving ball with collision checking

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,46 @@ Rectangle player { 30.0f, (float) windowHeight / 2.0f,  10.0f, 30.0f};
 const float playerSpeed = 5.0f;
 int playerScore = 0;
 
-// Opponent variables
-int opponentScore = 0;
-
 // Handles player movement
-void MovePlayer(Rectangle& rec)
+void MovePlayer()
 {
     // Move up if w pressed and not at the top of the screen
     // Move down if s pressed and not at the bottom of the screen
-    if (IsKeyDown(KEY_W) && rec.y > 0.0f)
-        rec.y -= playerSpeed;
-    else if (IsKeyDown(KEY_S) && rec.y < windowHeight - rec.height)
-        rec.y += playerSpeed;
+    if (IsKeyDown(KEY_W) && player.y > 0.0f)
+        player.y -= playerSpeed;
+    else if (IsKeyDown(KEY_S) && player.y < windowHeight - player.height)
+        player.y += playerSpeed;
+}
+
+// Opponent variables
+int opponentScore = 0;
+
+// Ball variables
+Vector2 ballPosition = { (float) windowWidth / 2.0f, (float) windowHeight / 2.0f };
+Vector2 ballSpeed { 5.0f, 4.0f };
+float ballRadius = 6.0f;
+
+// Makes the ball move and bounce off of the walls
+void MoveBall()
+{
+    // Move the ball
+    ballPosition.x += ballSpeed.x;
+    ballPosition.y += ballSpeed.y;
+
+    // If the ball hits a vertical edge, reverse (bounce) it away
+    if (ballPosition.x >= windowWidth - ballRadius || ballPosition.x <= ballRadius)
+        ballSpeed.x *= -1.0f;
+    // If the ball hits a horizontal edge, reverse (bounce) it away
+    if (ballPosition.y >= windowHeight - ballRadius || ballPosition.y <= ballRadius)
+        ballSpeed.y *= -1.0f;
+}
+
+// Checks for collisions and handles them
+void HandleCollision()
+{
+    // If the player collides with the ball, bounce the ball off of the player
+    if (CheckCollisionCircleRec(ballPosition, ballRadius, player))
+        ballSpeed.x *= -1.0f;
 }
 
 int main()
@@ -34,8 +62,15 @@ int main()
     {
         BeginDrawing();
         ClearBackground(BLACK);
-        MovePlayer(player);
+
+        MovePlayer();
         DrawRectangleRec(player, WHITE);
+
+        MoveBall();
+        DrawCircleV(ballPosition, ballRadius, WHITE);
+
+        HandleCollision();
+
         EndDrawing();
     }
 
